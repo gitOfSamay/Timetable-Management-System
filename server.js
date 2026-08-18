05545b63bcdc76e8795d41944aeb5b4647c1a113
Samay Jaiswal <jaissamay22@gmail.com>
2026-08-19 01:00:22 +0530

    Fix static frontend path

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const db = require("./db");
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(express.static("public"));
+const path = require("path");
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // ===============================
 // READ - Get complete timetable
